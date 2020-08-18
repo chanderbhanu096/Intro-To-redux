637957c7c08ad2d6f181c16160d151b2f783cb2a
Chander Bhanu <chanderbhanu096@gmail.com>
2020-08-18 22:23:59 +0530

    Datatype  intro

---
+++ Back to JavaScript Basics/Basics_variables.js
@@ -0,0 +1,22 @@
+/*
+var myName
+*/
+
+var myName = "Bhanu"
+
+myName = 8
+
+let ourName ="keeplearning"
+
+// const variable devlaration value can't be changes
+const pi = 3.4
+
+// variable with the changing value 
+let  pi2 = 1.3
+pi2 = 4
+
+// hows to skip quote character using  "\"
+
+var akhilesh = "let's escape  quote characterusi \" kerj sdwelwekjj "
+
+
